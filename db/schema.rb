bf69bd282c81bb5c2231e3643630cede8797a52c
SRobitzsch <Steven.Robitzsch@gmail.com>
2016-09-29 23:31:24 -0400

    Did some DB migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930010019) do
+ActiveRecord::Schema.define(version: 20160930022233) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "make"
     t.string   "color"
     t.integer  "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.decimal  "price",      precision: 10, scale: 2
   end
 
 end
